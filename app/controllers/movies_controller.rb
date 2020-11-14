4c005faff9a69c72b7494b19731a3c32465d64f3
rmmckeith <r.mckeith@me.com>
2020-11-14 10:34:53 -0800

    add characterization model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,11 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
     @fans = @movie.fans
+    @genres = @movie.genres
+
+    if current_user
+      @current_favorite = current_user.favorites.find_by(movie_id: @movie.id)
+    end
   end
 
   def edit
@@ -47,6 +52,6 @@ private
 
   def movie_params
     params.require(:movie).
-      permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name)
+      permit(:title, :description, :rating, :released_on, :total_gross, :cast, :director, :duration, :image_file_name, genre_ids: [])
   end
 end
